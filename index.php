3ba7fe7d0a82ef5d04b1aeddbdb9854f6f2a7dbc
Charla19 <charlap20.aps2a@gmail.com>
2022-08-05 15:31:21 +0300

    rm sub-header

--- index.php
+++ index.php
@@ -39,28 +39,7 @@ https://templatemo.com/tm-545-finance-business
     </div>  
     <!-- ***** Preloader End ***** -->
 
-    <!-- Header -->
-    <!--<div class="sub-header">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-xs-12">
-            <ul class="left-info">
-              <li><a href="#"><img src="assets/images/logo50.png" width="40" height="40"> Green Planet Communication</a></li>
-              <li><a href="#"><i class="fa fa-phone"></i>0033 6 12 34 56 78</a></li>
-            </ul>
-          </div>
-          <div class="col-md-4">
-            <ul class="right-icons">
-              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-              <li><a href="#"><i class="fa fa-behance"></i></a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>-->
-
+    <!-- Header starts here -->  
     <header class="">
       <nav class="navbar navbar-expand-lg">
         <div class="container">
