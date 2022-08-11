204cd7323b0794ba4d9281ab46f9494dfd793621
Charla19 <charlap20.aps2a@gmail.com>
2022-08-11 10:24:58 +0300

    spain lang

--- langues/es.php
+++ langues/es.php
@@ -35,17 +35,65 @@ $lang = array(
 	"banner4-h4-2" => " los efectos del tiempo.",
 	"banner4-p" => "Nos gusta hacer una gran explosión para impulsar su mensaje al frente. Pero lo que preferimos sobre todo es anclarlo de manera duradera en la mente de las personas.",
 	"banner4-btn" => "Aprende más",	
-	"slogan" => "Propulsa tus ideas de forma sostenible.",
+	"slogan" => "Propulsa tus ideas de forma sostenible",
+	//Request form
+	"request-h4" => "¿Solicitar una devolución de llamada ahora?",
+	"request-span" => "Mauris ut dapibus velit cras interdum nisl ac urna tempor mollis",
+	"request-a" => "Contáctenos",
+	//Services
+	"service-slogan" => "Propulsa tus ideas de forma sostenible",
+	"service1-h4" => "servicio 1",
+	"service1-p" => "Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.",
+	"service2-h4" => "servicio 2",
+	"service2-p" => "Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.",
+	"service3-h4" => "servicio 3",
+	"service3-p" => "Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.",
+	"service-btn" => "Descubrir",
+	//fun facts
+	"fun-fact-span" => "Propulsa tus ideas de forma sostenible",
+	"fun-fact-h2" => "Nuestras soluciones",
+	"fun-fact-em" => "para el crecimiento de su negocio",
+	"fun-fact-p" => "Pellentesque ultrices at turpis in vestibulum. Aenean pretium elit nec congue elementum. Nulla luctus laoreet porta. Maecenas at nisi tempus, porta metus vitae, faucibus augue.",
+	"fun-fact-br" => "Fusce et venenatis ex. Quisque varius, velit quis dictum sagittis, odio velit molestie nunc, ut posuere ante tortor ut neque.",
+	"fun-fact-btn" => "Más información",
+	"count-title1" => "Horas laborales",
+	"count-title2" => "Excelentes críticas",
+	"count-title3" => "Proyectos Realizados",
+	"count-title4" => "Premios ganados",
 	//about
 	"about-span" => "¿Quiénes somos?",
 	"about-h2" => "Descubriendo",
 	"about-h2-em" => "nuestra empresa",
 	"about-p" => "Nuestra misión es convertir la comunicación de su empresa en un vector de crecimiento sostenible.",
 	"about-btn" => "Descubrir",
+	//vision
+	"vision-h2" => "Proyección",
+	"vision-h2-em" => " y hoja de ruta",
+	"vision-span" => "Propulsa tus ideas de forma sostenible",
+	"vision-h2-em1" => "Nuestras visiones",
+	"vision-p" => "En tres años, consolidarse como un actor de referencia ofreciendo una gama completa de servicios de comunicación a las empresas.",
 	//testimonial
 	"testimonial-h2" => "Que dicen",
 	"testimonial-h2-em" => " de nosotros",
 	"testimonial-span" => "Las opiniones de nuestros mayores clientes",
+	"testimonial1-h4" => "Cliente 1",
+	"testimonial1-span" => "Función de cliente 1",
+	"testimonial1-p" => "Nulla ullamcorper, ipsum vel condimentum congue, mi odio vehicula tellus, sit amet malesuada justo sem sit amet quam. Pellentesque in sagittis lacus.",
+	"testimonial2-h4" => "Cliente 2",
+	"testimonial2-span" => "Función de cliente 2",
+	"testimonial2-p" => "Nulla ullamcorper, ipsum vel condimentum congue, mi odio vehicula tellus, sit amet malesuada justo sem sit amet quam. Pellentesque in sagittis lacus.",
+	"testimonial3-h4" => "Cliente 3",
+	"testimonial3-span" => "Función de cliente 3",
+	"testimonial3-p" => "Nulla ullamcorper, ipsum vel condimentum congue, mi odio vehicula tellus, sit amet malesuada justo sem sit amet quam. Pellentesque in sagittis lacus.",
+	//Call back
+	"callback-h2" => "Enviar ",
+	"callback-h2-em" => "un mensaje",
+	"callback-span" => "Etiam suscipit ante a odio consequat",
+	"callback-nom" => "Nombre completo",
+	"callback-mail" => "Correo electrónico",
+	"callback-subject" => "Objeto",
+	"callback-msg" => "Vuestro mensaje",
+	"callback-btn" => "Enviar",
 	//footer
 	// social networks
 	"footer1-h4" => "Nuestras redes sociales",
@@ -68,18 +116,21 @@ $lang = array(
 	"footer4-placeholder-fullname" => "Nombre completo",
 	"footer4-placeholder-mailAdress" => "Correo electrónico",
 	"footer4-placeholder-message" => "Vuestro mensaje",
+	"footer4-btn" => "Enviar",
 	//home page ends here 
 	//============================================================================================================================================================================================================================================================================================================================================================================================================
 	
 	//French language of about page about.php
 	//============================================================================================================================================================================================================================================================================================================================================================================================================
-	"title" => "Green Planet Communication| A proposito",
+	"title" => "A proposito",
 	//header (same for all pages) already declared
 	//section heading
 	"section-heading-h1" => "Conoce",
 	"section-heading-em" => " nuestra empresa",
 	"section-heading-span" => "Propulsa tus ideas de forma sostenible",
 	//Signature Green Planet Communication
+	"signature-h2" => "La firma de ",
+	"signature-h2-em" => "Green Planet Communication",
 	"signature1-span" => "Del griego antiguo harmonia, 'grupo de sonidos que van juntos'",
 	"signature1-h2" => "Armonía",
 	"signature1-p" => "Tu compañía funciona como una sinfonía donde cada nota, perfectamente tocada, se empareja melodiosamente con las demás. Green Planet Communication se toma el tiempo para escuchar su modo de operación y se sintoniza con sus valores. Luego llevamos su voz dentro de su empresa y en el corazón de su mercado.",
@@ -95,6 +146,7 @@ $lang = array(
 	//Principle values
 	"value-heading-h2" => "Los valores",
 	"value-heading-em" => " que apoyamos",
+	"value-heading-span" => "Convierte la comunicación de tu empresa en un vector de crecimiento sostenible",
 	"value1" => "Valor 1",
 	"value1-span" => "Sinónimo valor 1",
 	"value1-span" => "En tu profesión, eres un experto. Con tu equipo, tienes ideas relevantes e innovadoras. Tus clientes buscan tus habilidades, esas que te diferencian de tus competidores.",
@@ -104,45 +156,41 @@ $lang = array(
 	"value3" => "Valor 3",
 	"value3-span" => "Sinónimo valor 3",
 	"value3-p" => "Una buena comunicación significa asegurarse de que el mensaje correcto llegue a las personas adecuadas. Es sobre todo una cuestión de escuchar y adaptarse. Y ese es nuestro trabajo.",
-	//Visions
-	"vision-span" => "Convierte la comunicación de tu empresa en un vector de crecimiento sostenible",
-	"vision-h2-em" => "Proyección hacia el futuro",
-	"vision-p" => "En tres años, consolidarse como un actor de referencia ofreciendo una gama completa de servicios de comunicación a las empresas.",
-	"vision-p-br" => "Lograr una tasa promedio de satisfacción del cliente del 95% en los diversos KPI.",
-	"vision-p-br-br" => "Estas son las cifras que esperamos alcanzar en 10 años ->",
-	"vision-count-title1" => "Tasa de satisfacción del cliente",
-	"vision-count-title" => "Excelentes críticas",
-	"vision-count-title" => "Proyectos finalizados",
-	"vision-count-title" => "Premios ganados",
 	//testimonials (already declared)	
-	"testimonial1-h4" => "Cliente 1",
-	"testimonial1-span" => "Función de cliente 1",
-	"testimonial1-p" => '"Nulla ullamcorper, ipsum vel condimentum congue, mi odio vehicula tellus, sit amet malesuada justo sem sit amet quam. Pellentesque in sagittis lacus."',
-	"testimonial2-h4" => "Cliente 2",
-	"testimonial2-span" => "Función de cliente 2",
-	"testimonial2-p" => '"Nulla ullamcorper, ipsum vel condimentum congue, mi odio vehicula tellus, sit amet malesuada justo sem sit amet quam. Pellentesque in sagittis lacus."',
 	//footer (already  declared)
+	"footer2-link6" => "Firma",
+	"footer2-link7" => "Nuestros valores",
+	"footer3-link5" => "Inicio",
+	"footer3-link6" => "Servicios",
 	//============================================================================================================================================================================================================================================================================================================================================================================================================
 
 	// Services page
 	//============================================================================================================================================================================================================================================================================================================================================================================================================
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
+	"title2" => "Servicios",
+	//section heading
+	"section-heading-h1-2" => "Los servicios que ofrecemos",
+	"section-heading-span" => "Tenemos 20 años de experiencia",
+	//Services
+	"services-tabs1" => "Análisis de mercado",
+	"services-tabs2" => "Datos financieros",
+	"services-tabs3" => "Servicio de Contabilidad",
+	"services-tabs4" => "Evaluación global",
+	"tabs1-h4" => "Análisis de mercado",
+	"tabs1-p" => "Vivamus sed feugiat elit. Pellentesque pretium, massa at placerat vehicula, neque turpis pulvinar tortor, eget convallis lorem odio non tortor. Donec massa est, fermentum sit amet felis ac, maximus luctus elit. Vivamus aliquet, dolor id imperdiet imperdiet, dui diam aliquet dui, a euismod metus enim ac velit. Vivamus eu tristique odio, vel tristique quam.",
+	"tabs1-p-br-br" => "Proin eu molestie risus. Etiam suscipit pretium odio, at consectetur nisi. Sed ut dolor in augue cursus ultrices. Vivamus mauris turpis, auctor vel facilisis in, tincidunt vel diam. Sed vitae scelerisque orci. Nunc non magna orci. Aliquam commodo mauris ante.",
+	"tabs2-h4" => "Datos financieros",
+	"tabs2-p" => "Sed ut dolor in augue cursus ultrices. Vivamus mauris turpis, auctor vel facilisis in, tincidunt vel diam. Sed vitae scelerisque orci. Nunc non magna orci. Aliquam commodo mauris ante, quis posuere nibh vestibulum sit amet ",
+	"tabs2-p-br-br" => "Pellentesque pretium, massa at placerat vehicula, neque turpis pulvinar tortor, eget convallis lorem odio non tortor. Donec massa est, fermentum sit amet felis ac, maximus luctus elit. Vivamus aliquet, dolor id imperdiet imperdiet, dui diam aliquet dui, a euismod metus enim ac velit. Vivamus eu tristique odio, vel tristique quam.",
+	"tabs3-h4" => "Servicio de Contabilidad",
+	"tabs3-p" => "Mauris lobortis quam id dictum dignissim. Donec pellentesque erat dolor, cursus dapibus turpis hendrerit quis. Suspendisse at suscipit arcu. Nulla sed erat lectus. Nulla facilisi. In sit amet neque sapien. Donec scelerisque mi at gravida efficitur. Nunc lacinia a est eu malesuada. Curabitur eleifend elit sapien, sed pulvinar orci luctus eget. ",
+	"tabs3-p-br-br" => "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vel ultrices nulla, ac tincidunt eros. Aenean quis tellus velit. Praesent pretium justo non auctor condimentum.",
+	"tabs4-h4" => "Evaluación global",
+	"tabs4-p" => "Integer vehicula sapien quis dolor efficitur, eget molestie eros tempus. Curabitur sollicitudin, tortor at suscipit volutpat, nisi arcu aliquet dui, vitae semper sem turpis quis libero. Quisque vulputate lacinia nisl ac lobortis. Ut ultricies maximus turpis, in sollicitudin ligula posuere vel. ",
+	"tabs4-p-br-br" => "Donec finibus maximus neque, vitae egestas quam imperdiet nec. Proin nec mauris eu tortor consectetur tristique. Etiam suscipit ante a odio consequat, in ornare lacus suscipit. Cras ac libero massa. Quisque rhoncus velit feugiat vulputate mollis. Donec nisl eros, malesuada sed nisi id, condimentum condimentum quam.",
+	// call back (already declared)
+	//partners
+	//footer (already declared)
+	"footer2-link8" => "Servicios",
 	"" => "",
 	"" => "",
 	"" => "",
@@ -151,19 +199,26 @@ $lang = array(
 
 	//Contact page
 	//============================================================================================================================================================================================================================================================================================================================================================================================================
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
-	"" => "",
+	//header (already declared)
+	"title3" => "Contáctenos",
+	//section heading
+	"section-heading-h1-3" => "Contáctenos",
+	"section-heading-span" => "Tenemos 20 años de experiencia",
+	//Contact information
+	"contact-item1-h4" => "Teléfono",
+	"contact-item1-p" => "Vivamus ut tellus mi. Nulla nec cursus elit, id vulputate nec cursus augue.",
+	"Contact-item1-a" => "090-080-0760",
+	"contact-item2-h4" => "Correo electrónico",
+	"contact-item2-p" => "Vivamus ut tellus mi. Nulla nec cursus elit, id vulputate nec cursus augue.",
+	"Contact-item2-a" => "info@company.com",
+	"contact-item3-h4" => "Ubicación geográfica",
+	"contact-item3-p" => "Vivamus ut tellus mi. Nulla nec cursus elit, id vulputate nec cursus augue.",
+	"Contact-item3-a" => "Ver en Google Maps",
+	//Call back (already declared)
+	//Partners (already declared)
+	//footer (already declared)
+	"footer2-link9" => "Nuestros Contactos",
+	"footer2-link10" => "Ubicación",
 	"" => "",
 	"" => "",
 	"" => "",
